fc7228ece755d0d1c90acf47900cf7d3679cc526
Joao Rocha <jr007silva1306@gmail.com>
2023-01-10 16:36:12 +0000

    Changed LaneDetector!

--- LaneDetector.py
+++ LaneDetector.py
@@ -1,106 +1,78 @@
 import cv2
 import numpy as np
+import math
 
 def region(image):
     height, width = image.shape
 
+    """triangle = np.array([
+                       [(0, height), (int(width * 1 / 10), int(height / 2)), (int(width * 9 / 10), int(height / 2)), (width, height)]
+                       ])"""
+
     triangle = np.array([
-                       [(0, height), (int(width * 1 / 4), int(height / 3)), (int(width * 3 / 4), int(height / 3)), (width, height)]
+                       [(0, height), (0, int(height / 2)), (width, int(height / 2)), (width, height)]
                        ])
 
     mask = np.zeros_like(image)
 
     mask = cv2.fillPoly(mask, triangle, 255)
+
+    #cv2.imshow("Mask", mask)
+
     mask = cv2.bitwise_and(image, mask)
     return mask
 
-def display_lines(image, lines):
-    lines_image = np.zeros_like(image)
-    #make sure array isn't empty
+def drawHoughLines(image, lines):
     if lines is not None:
-        for line in lines:
-            x1, y1, x2, y2 = line
-            #draw lines on a black image
-            cv2.line(lines_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
-    return lines_image
+        for i in range(0, len(lines)):
+            rho = lines[i][0][0]
+            theta = lines[i][0][1]
+            a = math.cos(theta)
+            b = math.sin(theta)
+            x0 = a * rho
+            y0 = b * rho
+            pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
+            pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
+            cv2.line(image, pt1, pt2, (0,0,255), 3, cv2.LINE_AA)
+    return image
 
-def average(image, lines):
-    left = []
-    right = []
-
+def drawHoughLinesP(image, lines):
     if lines is not None:
-      for line in lines:
-        print(line)
-        x1, y1, x2, y2 = line.reshape(4)
-        #fit line to points, return slope and y-int
-        parameters = np.polyfit((x1, x2), (y1, y2), 1)
-        print(parameters)
-        slope = parameters[0]
-        y_int = parameters[1]
-        #lines on the right have positive slope, and lines on the left have neg slope
-        if slope < 0:
-            left.append((slope, y_int))
-        else:
-            right.append((slope, y_int))
+        for i in range(0, len(lines)):
+            l = lines[i][0]
+            cv2.line(image, (l[0], l[1]), (l[2], l[3]), (0,0,255), 3, cv2.LINE_AA)
+    return image
 
-    #takes average among all the columns (column0: slope, column1: y_int)
-    right_avg = np.average(right, axis=0)
-    left_avg = np.average(left, axis=0)
-    #create lines based on averages calculates
-    left_line = make_points(image, left_avg)
-    right_line = make_points(image, right_avg)
-    return np.array([left_line, right_line])
-
-def make_points(image, average):
-    print(average)
-    slope, y_int = average
-    y1 = image.shape[0]
-    #how long we want our lines to be --> 3/5 the size of the image
-    y2 = int(y1 * (3/5))
-    #determine algebraically
-    x1 = int((y1 - y_int) // slope)
-    x2 = int((y2 - y_int) // slope)
-    return np.array([x1, y1, x2, y2])
 
 class LaneDetector:
     def detect(self, frame):
         open_cv_image = np.array(frame)
         open_cv_image = open_cv_image[:, :, ::-1].copy()
 
-        gray = cv2.cvtColor(open_cv_image, cv2.COLOR_BGR2GRAY)
+        img = open_cv_image.copy()
+
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
         blur = cv2.GaussianBlur(gray, (5, 5), 0)
 
-        img_hsv = cv2.cvtColor(open_cv_image, cv2.COLOR_BGR2HSV)
-        lower_yellow = np.array([25, 100, 100], dtype='uint8')
-        upper_yellow = np.array([25, 255, 255], dtype='uint8')
-
-        mask_yellow = cv2.inRange(img_hsv, lower_yellow, upper_yellow)
-        mask_white = cv2.inRange(gray, 150, 255)
-        mask_ym = cv2.bitwise_or(mask_white, mask_yellow)
-        mask_ym_image = cv2.bitwise_and(blur, mask_ym)
-
-        region_image = region(mask_ym_image)
-
-        edges = cv2.Canny(region_image, 50, 150)
+        edges = cv2.Canny(blur, 150, 255, L2gradient = True)
 
         cv2.imshow("Edges", edges)
 
-        lines = cv2.HoughLinesP(edges, rho=2, theta=np.pi/180, threshold=100, minLineLength=40, maxLineGap=5)
+        region_image = region(edges)
 
-        if lines is None:
-            return
+        cv2.imshow("Region", region_image)
 
-        for line in lines:
-            x1, x2, y1, y2 = line[0]
-            cv2.line(open_cv_image, (x1, x2), (y1, y2), (255, 0, 0), 3)
+        lines = cv2.HoughLines(region_image, 1, np.pi / 180, 150, None, 0, 0)
+        linesP = cv2.HoughLinesP(region_image, rho=1, theta=np.pi/180, threshold=50, minLineLength=1, maxLineGap=100)
 
-        print('Lines No: ' + str(len(lines)))
+        imgCopy = img.copy()
 
-        averaged_lines = average(open_cv_image, lines)
-        black_lines = display_lines(open_cv_image, averaged_lines)
+        img = drawHoughLines(img, lines)
+        imgCopy = drawHoughLinesP(imgCopy, linesP)
 
-        lanes = cv2.addWeighted(open_cv_image, 0.8, black_lines, 1, 1)
+        cv2.imshow("Hough Lane Detection", img)
 
-        cv2.imshow("Lane Detection", lanes)
+        cv2.imshow("Hough Probabilistic Lane Detection", imgCopy)
+
         cv2.waitKey(1)
--- TestLaneDetector.py
+++ TestLaneDetector.py
@@ -1,77 +1,55 @@
 import cv2
 import numpy as np
+import math
 
 img = cv2.imread('screen.png')
 
 def region(image):
     height, width = image.shape
 
-    triangle = np.array([
-                       [(0, height - 100), (int(width * 1 / 3), int(height / 3)), (int(width * 2 / 3), int(height / 3)), (width, height - 100)]
-                       ])
-
     """triangle = np.array([
-                       [(0, height), (0, 0), (width, 0), (width, height)]
+                       [(0, height), (int(width * 1 / 10), int(height / 2)), (int(width * 9 / 10), int(height / 2)), (width, height)]
                        ])"""
+
+    triangle = np.array([
+                       [(0, height), (0, int(height / 2)), (width, int(height / 2)), (width, height)]
+                       ])
 
     mask = np.zeros_like(image)
 
     mask = cv2.fillPoly(mask, triangle, 255)
 
-    mask = cv2.bitwise_and(image, mask)
+    #cv2.imshow("Mask", mask)
 
-    cv2.imshow("Mask", mask)
+    mask = cv2.bitwise_and(image, mask)
     return mask
 
-def drawLine(image, line):
-    x1, x2, y1, y2 = line[0]
-    result = cv2.line(image, (x1, x2), (y1, y2), (255, 0, 0), 2)
-    return result
+def drawHoughLines(image, lines):
+    if lines is not None:
+        for i in range(0, len(lines)):
+            rho = lines[i][0][0]
+            theta = lines[i][0][1]
+            a = math.cos(theta)
+            b = math.sin(theta)
+            x0 = a * rho
+            y0 = b * rho
+            pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
+            pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
+            cv2.line(image, pt1, pt2, (0,0,255), 3, cv2.LINE_AA)
+    return image
 
-def draw_lines(img, lines, color=[255, 0, 0], thickness=7):
-    x_bottom_pos = []
-    x_upper_pos = []
-    x_bottom_neg = []
-    x_upper_neg = []
-
-    y_bottom = 540
-    y_upper = 315
-
-    slope = 0
-    b = 0
-
-    for line in lines:
-        for x1,y1,x2,y2 in line:
-            # test and filter values to slope
-            if ((y2-y1)/(x2-x1)) > 0.5 and ((y2-y1)/(x2-x1)) < 0.8:
-
-                slope = ((y2-y1)/(x2-x1))
-                b = y1 - slope*x1
-
-                x_bottom_pos.append((y_bottom - b)/slope)
-                x_upper_pos.append((y_upper - b)/slope)
-
-            elif ((y2-y1)/(x2-x1)) < -0.5 and ((y2-y1)/(x2-x1)) > -0.8:
-
-                slope = ((y2-y1)/(x2-x1))
-                b = y1 - slope*x1
-
-                x_bottom_neg.append((y_bottom - b)/slope)
-                x_upper_neg.append((y_upper - b)/slope)
-
-    # a new 2d array with means
-    lines_mean = np.array([[int(np.mean(x_bottom_pos)), int(np.mean(y_bottom)), int(np.mean(x_upper_pos)), int(np.mean(y_upper))],
-                            [int(np.mean(x_bottom_neg)), int(np.mean(y_bottom)), int(np.mean(x_upper_neg)), int(np.mean(y_upper))]])
-
-    # Draw the lines
-    for i in range(len(lines_mean)):
-        cv2.line(img, (lines_mean[i, 0], lines_mean[i, 1]), (lines_mean[i, 2], lines_mean[i, 3]), color, thickness)
+def drawHoughLinesP(image, lines):
+    if lines is not None:
+        for i in range(0, len(lines)):
+            l = lines[i][0]
+            cv2.line(image, (l[0], l[1]), (l[2], l[3]), (0,0,255), 3, cv2.LINE_AA)
+    return image
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 blur = cv2.GaussianBlur(gray, (5, 5), 0)
 
-edges = cv2.Canny(blur, 50, 150)
+edges = cv2.Canny(blur, 150, 255, L2gradient = True)
 
 cv2.imshow("Edges", edges)
 
@@ -79,14 +57,16 @@ region_image = region(edges)
 
 cv2.imshow("Region", region_image)
 
-lines = cv2.HoughLinesP(region_image, rho=1, theta=np.pi/180, threshold=100, minLineLength=100, maxLineGap=1000)
+lines = cv2.HoughLines(region_image, 1, np.pi / 180, 150, None, 0, 0)
+linesP = cv2.HoughLinesP(region_image, rho=1, theta=np.pi/180, threshold=50, minLineLength=1, maxLineGap=100)
 
-for line in lines:
-    img = drawLine(img, line)
+imgCopy = img.copy()
 
-print('Lines No: ' + str(len(lines)))
+img = drawHoughLines(img, lines)
+imgCopy = drawHoughLinesP(imgCopy, linesP)
 
-draw_lines(img, lines, color=[0, 0, 255], thickness=2)
+cv2.imshow("Hough Lane Detection", img)
 
-cv2.imshow("Lane Detection", img)
+cv2.imshow("Hough Probabilistic Lane Detection", imgCopy)
+
 cv2.waitKey(0)
